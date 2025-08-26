1991999bec7bf4b59f4f317a8803b5c05554691c
ExistingPerson <164225044+ExistingPerson08@users.noreply.github.com>
2025-08-26 14:57:48 +0200

    Remove unused apps

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,7 +3,8 @@
 set -ouex pipefail
 
 # Install COSMIC desktop and exclude non-cosmic apps
-dnf install -y @cosmic-desktop @cosmic-desktop-apps --exclude=okular,rhythmbox,thunderbird,nheko,ark
+dnf install -y @cosmic-desktop @cosmic-desktop-apps --exclude=okular,rhythmbox,thunderbird,nheko,ark,gnome-calculator
 
-
+# Remove unused Bluefin-dx apps
+dnf remove sysprof
 
