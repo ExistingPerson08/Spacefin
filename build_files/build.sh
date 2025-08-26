c1884c049cc0d89e2148768a286c33d50d38c046
ExistingPerson <164225044+ExistingPerson08@users.noreply.github.com>
2025-08-26 12:42:54 +0200

    Exclude ark

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,5 +3,7 @@
 set -ouex pipefail
 
 # Install COSMIC desktop and exclude non-cosmic apps
-dnf install -y @cosmic-desktop @cosmic-desktop-apps --exclude=okular,rhythmbox,thunderbird,nheko
+dnf install -y @cosmic-desktop @cosmic-desktop-apps --exclude=okular,rhythmbox,thunderbird,nheko,ark
+
+
 
