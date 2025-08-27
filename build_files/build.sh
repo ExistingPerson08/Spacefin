2d6b6cbbd0a85f31c5dd9a4e6ee7a1782ab2c39f
ExistingPerson <164225044+ExistingPerson08@users.noreply.github.com>
2025-08-27 11:03:03 +0200

    Add -y to copr enable

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,7 +3,7 @@
 set -ouex pipefail
 
 # Install COSMIC desktop and exclude non-cosmic apps
-dnf copr enable ryanabx/cosmic-epoch
+dnf copr enable -y ryanabx/cosmic-epoch
 dnf install cosmic-desktop --exclude=okular,rhythmbox,thunderbird,nheko,ark,gnome-calculator
 
 # Remove unused Bluefin-dx apps
