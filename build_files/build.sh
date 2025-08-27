e7f09688166b23bec4a23117a44782e498b6e736
ExistingPerson <164225044+ExistingPerson08@users.noreply.github.com>
2025-08-27 16:58:24 +0200

    Add support for main and experimental image

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,14 +2,23 @@
 
 set -ouex pipefail
 
-# Install COSMIC desktop and exclude non-cosmic apps
-dnf copr enable -y ryanabx/cosmic-epoch
-dnf install -y cosmic-desktop --exclude=okular,rhythmbox,thunderbird,nheko,ark,gnome-calculator
+case "$1" in
+    "main")
+        # Using tagged Cosmic desktop in main image
+        dnf install -y @cosmic-desktop @cosmic-desktop-apps --exclude=okular,rhythmbox,thunderbird,nheko,ark,gnome-calculator
+        ;;
+    "exp")
+        # Using latest (nightly) Cosmic desktop in exp image
+        dnf copr enable -y ryanabx/cosmic-epoch
+        dnf install -y cosmic-desktop --exclude=okular,rhythmbox,thunderbird,nheko,ark,gnome-calculator
+
+        # Remove unused Bluefin-dx apps
+        dnf remove -y sysprof
+        ;;
+esac
 
 # Install additional GNOME apps
 # (native version is better than flatpak)
 dnf install -y showtime gnome-firmware
 
-# Remove unused Bluefin-dx apps
-dnf remove -y sysprof
 
