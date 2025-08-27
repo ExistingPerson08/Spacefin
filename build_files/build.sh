8437a6338a492a24eaf2a42b1ce22ff97b34ae4a
ExistingPerson <164225044+ExistingPerson08@users.noreply.github.com>
2025-08-27 11:17:20 +0200

    Fix: qualculate cant be excluded

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,7 +4,7 @@ set -ouex pipefail
 
 # Install COSMIC desktop and exclude non-cosmic apps
 dnf copr enable -y ryanabx/cosmic-epoch
-dnf install -y cosmic-desktop --exclude=okular,rhythmbox,thunderbird,nheko,ark,gnome-calculator,qalculate
+dnf install -y cosmic-desktop --exclude=okular,rhythmbox,thunderbird,nheko,ark,gnome-calculator
 
 # Remove unused Bluefin-dx apps
 dnf remove -y sysprof
