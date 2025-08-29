1517497d33bce8e1dccbee8b86fe54b47ba93111
ExistingPerson <164225044+ExistingPerson08@users.noreply.github.com>
2025-08-29 11:36:55 +0200

    Add building hybrid images

--- build_files/build.sh
+++ build_files/build.sh
@@ -7,6 +7,21 @@ case "$1" in
         # Using tagged Cosmic desktop in main image
         dnf install -y @cosmic-desktop @cosmic-desktop-apps --exclude=okular,rhythmbox,thunderbird,nheko,ark,gnome-calculator
         ;;
+    "hybrid")
+        # Using tagged Cosmic  desktop in hybrid image
+        dnf install -y @cosmic-desktop @cosmic-desktop-apps --exclude=okular,rhythmbox,thunderbird,nheko,ark,gnome-calculator
+
+        # Add to justfile
+        echo "import \"/usr/share/spacefin/just/spacefin.just\"" >>/usr/share/ublue-os/justfile
+
+        # Remove incompatible just recipes
+        for recipe in "devmode" "toggle-devmode" ; do
+          if ! grep -l "^$recipe:" /usr/share/ublue-os/just/*.just | grep -q .; then
+            echo "Skipping"
+          fi
+          sed -i "s/^$recipe:/_$recipe:/" /usr/share/ublue-os/just/*.just
+        done
+        ;;
     "exp")
         # Using latest (nightly) Cosmic desktop in exp image
         dnf copr enable -y ryanabx/cosmic-epoch
@@ -14,23 +29,14 @@ case "$1" in
 
         # Remove unused Bluefin-dx apps
         dnf remove -y sysprof
+
+        # Add to justfile
+        echo "import \"/usr/share/spacefin/just/spacefin.just\"" >>/usr/share/ublue-os/justfile
         ;;
 esac
 
 # Install additional GNOME apps
 # (native version is better than flatpak)
 dnf install -y showtime gnome-firmware
-
-# Add to justfile
-echo "import \"/usr/share/spacefin/just/spacefin.just\"" >>/usr/share/ublue-os/justfile
-
-# Remove incompatible just recipes
-for recipe in "devmode" "toggle-devmode" ; do
-  if ! grep -l "^$recipe:" /usr/share/ublue-os/just/*.just | grep -q .; then
-    echo "Error: Recipe $recipe not found in any just file"
-    exit 1
-  fi
-  sed -i "s/^$recipe:/_$recipe:/" /usr/share/ublue-os/just/*.just
-done
 
 
