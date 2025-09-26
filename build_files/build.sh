9090d299b3e1e5bab3a7af590060200b2fcd72a2
ExistingPerson <164225044+ExistingPerson08@users.noreply.github.com>
2025-09-26 17:55:47 +0000

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,26 +5,16 @@ set -ouex pipefail
 case "$1" in
     "main")
         # Using tagged Cosmic desktop in main image
-        # dnf install -y @cosmic-desktop @cosmic-desktop-apps --exclude=okular,rhythmbox,thunderbird,nheko,ark,gnome-calculator
-        # dnf install -y --skip-unavailable gnome-backgrounds fastfetch rclone restic samba samba-dcerpc samba-ldb-ldap-modules samba-winbind-clients samba-winbind-modules tailscale wireguard-tools zsh fish firewall-config
-        # Use latest Cosmic desktop for testing - remove this in Beta
-        dnf copr enable -y ryanabx/cosmic-epoch
-        dnf install -y cosmic-desktop --exclude=okular,rhythmbox,thunderbird,nheko,ark,gnome-calculator
+        dnf install -y @cosmic-desktop @cosmic-desktop-apps --exclude=okular,rhythmbox,thunderbird,nheko,ark,gnome-calculator
+        
+        # Remove GNOME desktop and duplicated apps
+        dnf5 -y group remove gnome-desktop
+        dnf remove gdm gnome-shell gnome-session
+        dnf remove gnome-tweaks nautilus
         ;;
     "hybrid")
         # Using tagged Cosmic  desktop in hybrid image
         dnf install -y @cosmic-desktop @cosmic-desktop-apps --exclude=okular,rhythmbox,thunderbird,nheko,ark,gnome-calculator
-
-        # Remove incompatible just recipes
-        for recipe in "devmode" "toggle-devmode" "install-system-flatpaks" ; do
-          if ! grep -l "^$recipe:" /usr/share/ublue-os/just/*.just | grep -q .; then
-            echo "Skipping"
-          fi
-          sed -i "s/^$recipe:/_$recipe:/" /usr/share/ublue-os/just/*.just
-        done
-
-        # Add to justfile
-        echo "import \"/usr/share/spacefin/just/spacefin.just\"" >>/usr/share/ublue-os/justfile
         ;;
     "exp")
         # Using latest (nightly) Cosmic desktop in exp image
@@ -41,19 +31,19 @@ case "$1" in
           fi
           sed -i "s/^$recipe:/_$recipe:/" /usr/share/ublue-os/just/*.just
         done
-
-        # Add to justfile
-        echo "import \"/usr/share/spacefin/just/spacefin.just\"" >>/usr/share/ublue-os/justfile
-
-        # Remove incompatible just recipes
-        for recipe in "devmode" "toggle-devmode" ; do
-          if ! grep -l "^$recipe:" /usr/share/ublue-os/just/*.just | grep -q .; then
-            echo "Skipping"
-          fi
-          sed -i "s/^$recipe:/_$recipe:/" /usr/share/ublue-os/just/*.just
-        done
         ;;
 esac
+
+# Remove incompatible just recipes
+for recipe in "devmode" "toggle-devmode" "install-system-flatpaks" ; do
+  if ! grep -l "^$recipe:" /usr/share/ublue-os/just/*.just | grep -q .; then
+    echo "Skipping"
+  fi
+  sed -i "s/^$recipe:/_$recipe:/" /usr/share/ublue-os/just/*.just
+done
+
+# Add to justfile
+echo "import \"/usr/share/spacefin/just/spacefin.just\"" >>/usr/share/ublue-os/justfile
 
 # Install additional GNOME apps
 # (native version is better than flatpak)
