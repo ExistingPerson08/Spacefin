a71a7bcf8b5f145dd947625089edef2742b26a75
ExistingPerson <164225044+ExistingPerson08@users.noreply.github.com>
2025-10-18 20:53:50 +0200

    Optimize build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,37 +2,38 @@
 
 set -ouex pipefail
 
-dnf install -y --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
-dnf install -y terra-release-extras
-dnf copr enable -y ublue-os/packages
-dnf copr enable -y ublue-os/staging
-dnf copr enable -y bazzite-org/bazzite
-dnf copr enable -y bazzite-org/bazzite-multilib
+dnf5 install -y --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
+dnf5 install -y terra-release-extras
+dnf5 copr enable -y \
+    ublue-os/packages \
+    ublue-os/staging \
+    bazzite-org/bazzite \
+    bazzite-org/bazzite-multilib
 
 case "$1" in
     "main")
         IMAGE_NAME="main"
         
         # Using tagged Cosmic desktop in main image
-        dnf install -y @cosmic-desktop @cosmic-desktop-apps --exclude=okular,rhythmbox,thunderbird,nheko,ark,gnome-calculator
+        dnf5 install -y @cosmic-desktop @cosmic-desktop-apps --exclude=okular,rhythmbox,thunderbird,nheko,ark,gnome-calculator
 
-        dnf copr enable -y kylegospo/system76-scheduler
-        dnf install -y system76-scheduler
+        dnf5 copr enable -y kylegospo/system76-scheduler
+        dnf5 install -y system76-scheduler
         
         systemctl enable cosmic-greeter
         systemctl enable com.system76.Scheduler
 
         # Cleanup
-        dnf copr remove -y kylegospo/system76-scheduler
+        dnf5 copr remove -y kylegospo/system76-scheduler
         ;;
     "hybrid")
         IMAGE_NAME="hybrid"
         
         # Using tagged Cosmic  desktop in hybrid image
-        dnf install -y @cosmic-desktop @cosmic-desktop-apps --exclude=okular,rhythmbox,thunderbird,nheko,ark,gnome-calculator
+        dnf5 install -y @cosmic-desktop @cosmic-desktop-apps --exclude=okular,rhythmbox,thunderbird,nheko,ark,gnome-calculator
 
         # Setup GNOME
-        dnf remove -y gnome-classic-session gnome-tour gnome-extensions-app gnome-system-monitor gnome-software gnome-software-rpm-ostree gnome-tweaks gnome-shell-extension-apps-menu gnome-shell-extension-background-logo yelp
+        dnf5 remove -y gnome-classic-session gnome-tour gnome-extensions-app gnome-system-monitor gnome-software gnome-software-rpm-ostree gnome-tweaks gnome-shell-extension-apps-menu gnome-shell-extension-background-logo yelp
         dnf5 -y swap --repo terra-extras gnome-shell gnome-shell
         dnf5 versionlock add gnome-shell
         dnf5 -y install \
@@ -54,15 +55,15 @@ case "$1" in
     "exp")
         IMAGE_NAME="experimental"
         # Using latest (nightly) Cosmic desktop in exp image
-        dnf copr enable -y ryanabx/cosmic-epoch
-        dnf install -y cosmic-desktop --exclude=okular,rhythmbox,thunderbird,nheko,ark,gnome-calculator
+        dnf5 copr enable -y ryanabx/cosmic-epoch
+        dnf5 install -y cosmic-desktop --exclude=okular,rhythmbox,thunderbird,nheko,ark,gnome-calculator
 
         # Add niri
-        dnf copr enable -y yalter/niri 
-        dnf install -y niri
+        dnf5 copr enable -y yalter/niri 
+        dnf5 install -y niri
 
         # Setup GNOME
-        dnf remove -y gnome-classic-session gnome-tour gnome-extensions-app gnome-system-monitor gnome-software gnome-software-rpm-ostree gnome-tweaks gnome-shell-extension-apps-menu gnome-shell-extension-background-logo yelp
+        dnf5 remove -y gnome-classic-session gnome-tour gnome-extensions-app gnome-system-monitor gnome-software gnome-software-rpm-ostree gnome-tweaks gnome-shell-extension-apps-menu gnome-shell-extension-background-logo yelp
         dnf5 -y swap --repo terra-extras gnome-shell gnome-shell
         dnf5 versionlock add gnome-shell
         dnf5 -y install \
@@ -82,14 +83,22 @@ case "$1" in
           --exclude=gnome-extensions-app
 
         # Install apps for experimental image
-        dnf install -y youtube-music zed codium codium-marketplace
-        dnf install -y qemu qemu-char-spice qemu-device-display-virtio-gpu qemu-device-display-virtio-vga qemu-device-usb-redirect qemu-img qemu-system-x86-core qemu-user-binfmt qemu-user-static
-        dnf install -y flatpak-builder virt-manager virt-v2v virt-viewer libvirt libvirt-nss
-        dnf install -y waydroid scrcpy
+        dnf5 install -y \
+            youtube-music \
+            zed \
+            codium \
+            codium-marketplace \
+            flatpak-builder \
+            gnome-boxes \
+            zsh \
+            restic \
+            rclone \
+            waydroid \
+            scrcpy
 
         # Cleanup
-        dnf copr remove -y yalter/niri 
-        dnf copr remove -y ryanabx/cosmic-epoch
+        dnf5 copr remove -y yalter/niri 
+        dnf5 copr remove -y ryanabx/cosmic-epoch
         ;;
 esac
 
@@ -137,20 +146,16 @@ done
 echo "import \"/usr/share/spacefin/just/spacefin.just\"" >>/usr/share/ublue-os/justfile
 
 # Install additional packages
-dnf install -y fastfetch ublue-brew ublue-motd firewall-config fish bluefin-cli-logos
+dnf5 install -y fastfetch ublue-brew ublue-motd firewall-config fish bluefin-cli-logos showtime gnome-firmware
 dnf5 install -y --enable-repo=copr:copr.fedorainfracloud.org:ublue-os:packages ublue-os-media-automount-udev
-dnf install -y steamdeck-backgrounds gnome-backgrounds
-
-# Install additional GNOME apps
-# (native version is better than flatpak)
-dnf install -y showtime gnome-firmware
+dnf5 install -y steamdeck-backgrounds gnome-backgrounds
 
 # Use ghostty instead of ptyxis
-dnf install -y ghostty
-dnf remove -y ptyxis
+dnf5 install -y ghostty
+dnf5 remove -y ptyxis
 
 # Remove Bazaar due old version
-dnf remove -y bazaar
+dnf5 remove -y bazaar
 
 # Add Flatpak preinstall
 dnf5 -y copr enable ublue-os/flatpak-test
@@ -187,9 +192,10 @@ EOF
 for repo in terra terra-extras; do
     sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/$repo.repo
 done
-dnf copr remove -y bazzite-org/bazzite
-dnf copr remove -y bazzite-org/bazzite-multilib
-dnf copr remove -y ublue-os/packages
-dnf copr remove -y ublue-os/staging
-dnf remove -y htop nvtop firefox firefox-langpacks toolbox
-dnf clean all -y
+dnf5 copr remove -y \
+    ublue-os/packages \
+    ublue-os/staging \
+    bazzite-org/bazzite \
+    bazzite-org/bazzite-multilib
+dnf5 remove -y htop nvtop firefox firefox-langpacks toolbox
+dnf5 clean all -y
