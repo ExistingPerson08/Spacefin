571e39d63c133fcd5562ae2d873db81fb62ead9e
ExistingPerson <164225044+ExistingPerson08@users.noreply.github.com>
2025-10-18 21:00:33 +0200

    Fix syntax errors

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,11 +4,10 @@ set -ouex pipefail
 
 dnf5 install -y --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
 dnf5 install -y terra-release-extras
-dnf5 copr enable -y \
-    ublue-os/packages \
-    ublue-os/staging \
-    bazzite-org/bazzite \
-    bazzite-org/bazzite-multilib
+dnf5 -y copr enable ublue-os/packages
+dnf5 -y copr enable ublue-os/staging
+dnf5 -y copr enable bazzite-org/bazzite
+dnf5 -y copr enable bazzite-org/bazzite-multilib
 
 case "$1" in
     "main")
@@ -192,10 +191,9 @@ EOF
 for repo in terra terra-extras; do
     sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/$repo.repo
 done
-dnf5 copr remove -y \
-    ublue-os/packages \
-    ublue-os/staging \
-    bazzite-org/bazzite \
-    bazzite-org/bazzite-multilib
+dnf5 -y copr remove ublue-os/packages
+dnf5 -y copr remove ublue-os/staging
+dnf5 -y copr remove bazzite-org/bazzite
+dnf5 -y copr remove bazzite-org/bazzite-multilib
 dnf5 remove -y htop nvtop firefox firefox-langpacks toolbox
 dnf5 clean all -y
