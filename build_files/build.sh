e7da4a0218dc43332ec79368de2fdecb2182e24a
ExistingPerson <164225044+ExistingPerson08@users.noreply.github.com>
2025-10-19 20:06:51 +0200

    Remove fedora-chromium-config

--- build_files/build.sh
+++ build_files/build.sh
@@ -234,7 +234,7 @@ dnf5 -y copr remove ublue-os/packages
 dnf5 -y copr remove ublue-os/staging
 dnf5 -y copr remove bazzite-org/bazzite
 dnf5 -y copr remove bazzite-org/bazzite-multilib
-dnf5 remove -y htop nvtop firefox firefox-langpacks toolbox clapper
+dnf5 remove -y htop nvtop firefox firefox-langpacks toolbox clapper fedora-bookmarks fedora-chromium-config fedora-chromium-config-gnome
 dnf5 clean all -y
 
 # Finalize
