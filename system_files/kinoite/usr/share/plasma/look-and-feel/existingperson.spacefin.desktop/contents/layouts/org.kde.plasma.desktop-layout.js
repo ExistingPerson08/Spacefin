3b04ef78d07ce4fe7aec5483ddd528d3e9d02220
ExistingPerson <164225044+ExistingPerson08@users.noreply.github.com>
2025-11-17 11:51:42 +0100

    Add desktop layout configuration for Plasma

---
+++ system_files/kinoite/usr/share/plasma/look-and-feel/existingperson.spacefin.desktop/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -0,0 +1,118 @@
+var plasma = getApiVersion(1);
+
+var layout = {
+    "desktops": [
+        {
+            "applets": [
+            ],
+            "config": {
+                "/": {
+                    "ItemGeometries-1920x1080": "",
+                    "ItemGeometriesHorizontal": "",
+                    "formfactor": "0",
+                    "immutability": "1",
+                    "lastScreen": "0",
+                    "wallpaperplugin": "org.kde.image"
+                }
+            },
+            "wallpaperPlugin": "org.kde.image"
+        },
+        {
+            "applets": [
+            ],
+            "config": {
+                "/": {
+                    "formfactor": "0",
+                    "immutability": "1",
+                    "lastScreen": "1",
+                    "wallpaperplugin": "org.kde.image"
+                }
+            },
+            "wallpaperPlugin": "org.kde.image"
+        }
+    ],
+    "panels": [
+        {
+            "alignment": "center",
+            "applets": [
+                {
+                    "config": {
+                        "/": {
+                            "PreloadWeight": "100",
+                            "popupHeight": "507",
+                            "popupWidth": "571"
+                        },
+                        "/ConfigDialog": {
+                            "DialogHeight": "630",
+                            "DialogWidth": "810"
+                        },
+                        "/General": {
+                            "alphaSort": "true",
+                            "displayPosition": "0",
+                            "favoritesPortedToKAstats": "true",
+                            "icon": "fedora-logo-icon",
+                            "systemFavorites": "suspend\\,hibernate\\,reboot\\,shutdown"
+                        }
+                    },
+                    "plugin": "io.github.jinliu.kickon"
+                },
+                {
+                    "config": {
+                    },
+                    "plugin": "org.kde.plasma.marginsseparator"
+                },
+                {
+                    "config": {
+                        "/ConfigDialog": {
+                            "DialogHeight": "630",
+                            "DialogWidth": "810"
+                        },
+                        "/General": {
+                            "groupingStrategy": "0",
+                            "launchers": "applications:systemsettings.desktop,applications:org.kde.discover.desktop,preferred://filemanager,file:///var/lib/flatpak/app/io.github.kolunmi.Bazaar/current/active/export/share/applications/io.github.kolunmi.Bazaar.desktop",
+                            "taskMaxWidth": "Narrow"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.taskmanager"
+                },
+                {
+                    "config": {
+                    },
+                    "plugin": "org.kde.plasma.systemtray"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "popupHeight": "400",
+                            "popupWidth": "560"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.digitalclock"
+                },
+                {
+                    "config": {
+                    },
+                    "plugin": "org.kde.plasma.showdesktop"
+                }
+            ],
+            "config": {
+                "/": {
+                    "formfactor": "2",
+                    "immutability": "1",
+                    "lastScreen": "0",
+                    "wallpaperplugin": "org.kde.image"
+                }
+            },
+            "height": 2,
+            "hiding": "dodgewindows",
+            "location": "bottom",
+            "maximumLength": 106.66666666666667,
+            "minimumLength": 106.66666666666667,
+            "offset": 0
+        }
+    ],
+    "serializationFormatVersion": "1"
+}
+;
+
+plasma.loadSerializedLayout(layout);
