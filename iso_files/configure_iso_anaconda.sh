1520a3992832ffd12d331fa0f78cc28c413d4ba0
ExistingPerson <dedek.vlastimil2@proton.me>
2025-12-18 20:39:17 +0100

    Setup iso build using Titanoboa

---
+++ iso_files/configure_iso_anaconda.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -eoux pipefail
+
+# Remove packages to save space
+dnf remove -y ublue-brew || true
+
+# Disable services not needed in live environment
+systemctl disable rpm-ostree-countme.service
+systemctl disable bootloader-update.service
+
+# Install Anaconda for installation
+dnf install -y anaconda-live libblockdev-btrfs
