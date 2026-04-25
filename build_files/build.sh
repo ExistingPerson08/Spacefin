97260f152b9be24dc875e54d52c2cdecd6ee6781
ExistingPerson <164225044+ExistingPerson08@users.noreply.github.com>
2026-04-25 07:52:17 +0000

    Use quickshell-git

--- build_files/build.sh
+++ build_files/build.sh
@@ -124,7 +124,7 @@ case "$1" in
         DE_NAME="niri"
 
         # Install and setup niri
-        pacman -S --noconfirm niri dms-shell-git mate-polkit wl-clipboard dgop matugen
+        pacman -S --noconfirm niri dms-shell-git mate-polkit wl-clipboard dgop matugen quickshell-git
 
         # Install aditional packages and dependencies
         pacman -S --noconfirm \
