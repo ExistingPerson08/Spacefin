be610a098d0b768a0e53a72ae0918d8c99ddde96
ExistingPerson <164225044+ExistingPerson08@users.noreply.github.com>
2026-07-05 22:20:57 +0200

    Shut up

--- build_files/from-source/install-rules.sh
+++ build_files/from-source/install-rules.sh
@@ -3,7 +3,7 @@
 set -ouex pipefail
 
 git clone https://github.com/ExistingPerson08/ExistingRules /etc/ananicy.d
-rm /etc/ananicy.d/LICENCE
-rm /etc/ananicy.d/PKGBUILD
-rm /etc/ananicy.d/README.md
+rm -f /etc/ananicy.d/LICENCE
+rm -f /etc/ananicy.d/PKGBUILD
+rm -f /etc/ananicy.d/README.md
 rm -r /etc/ananicy.d/.git
