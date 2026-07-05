602c6250cc814e5138879417a4df9fb5a7f33d6d
ExistingPerson <dedek.vlastimil2@proton.me>
2026-07-05 20:59:24 +0200

    I forgot...

--- build_files/build.sh
+++ build_files/build.sh
@@ -13,6 +13,7 @@ sed -i 's/^#ParallelDownloads.*/ParallelDownloads = 10/' /etc/pacman.conf
 /ctx/from-source/install-rules.sh
 /ctx/from-source/install-bazaar.sh
 /ctx/build-essentials.sh
+/ctx/build-config.sh
 /ctx/finalize.sh
 
 dracut --force "$(find /usr/lib/modules -maxdepth 1 -type d | grep -v -E "*.img" | tail -n 1)/initramfs.img"
