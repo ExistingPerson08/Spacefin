343d094b21b1ff0dbe0dfc76e50e60602bd3199f
ExistingPerson <164225044+ExistingPerson08@users.noreply.github.com>
2026-07-05 22:47:01 +0200

    I am stupid
    I misspelled it

--- build_files/from-source/install-rules.sh
+++ build_files/from-source/install-rules.sh
@@ -3,7 +3,7 @@
 set -ouex pipefail
 
 git clone https://github.com/ExistingPerson08/ExistingRules /etc/ananicy.d
-rm -f /etc/ananicy.d/LICENCE
+rm -f /etc/ananicy.d/LICENSE
 rm -f /etc/ananicy.d/PKGBUILD
 rm -f /etc/ananicy.d/README.md
 rm -r /etc/ananicy.d/.git
