4ac622b9f482e96fb69e4e52a3078ca08eac5610
ExistingPerson <164225044+ExistingPerson08@users.noreply.github.com>
2026-07-10 12:24:37 +0200

    Try to fix v2

--- build_files/from-source/install-aur.sh
+++ build_files/from-source/install-aur.sh
@@ -39,6 +39,7 @@ VERSION=$(curl -s "https://api.github.com/repos/AvengeMedia/dankcalendar/release
 URL_REL="https://github.com/AvengeMedia/dankcalendar/releases/download/v${VERSION}"
 URL_RAW="https://raw.githubusercontent.com/AvengeMedia/dankcalendar/v${VERSION}"
 DESKTOP_ID="com.danklinux.dankcalendar"
+
 TMP_DIR=$(mktemp -d)
 cd "$TMP_DIR"
 
@@ -49,22 +50,25 @@ curl -L -O "${URL_RAW}/LICENSE"
 curl -L -O "${URL_RAW}/README.md"
 curl -L -o "${DESKTOP_ID}.desktop" "${URL_RAW}/assets/${DESKTOP_ID}.desktop"
 curl -L -o "dcal.service" "${URL_RAW}/assets/systemd/dcal.service"
+curl -L -o "dankcalendar.svg" "${URL_RAW}/assets/dankcalendar.svg"
 
 gunzip "dcal-linux.gz"
 tar -xzf "dankcalendar-qml-${VERSION}.tar.gz"
-tar -xzf "dcal-completions-${VERSION}.tar.gz"
+tar -xzf "dcal-completions-${VERSION}.tar.gz" --strip-components=1 2>/dev/null || tar -xzf "dcal-completions-${VERSION}.tar.gz"
 
 install -Dm755 "dcal-linux" "/usr/bin/dcal"
-install -Dm644 "dcal"  "/usr/share/bash-completion/completions/dcal"
+install -Dm644 "dcal" "/usr/share/bash-completion/completions/dcal"
 install -Dm644 "_dcal" "/usr/share/zsh/site-functions/_dcal"
 install -Dm644 "dcal.fish" "/usr/share/fish/vendor_completions.d/dcal.fish"
 
 install -dm755 "/usr/share/quickshell/dankcal"
 cp -r dankcal/. "/usr/share/quickshell/dankcal/"
-install -Dm644 "dankcal/assets/dankcalendar.svg" "/usr/share/icons/hicolor/scalable/apps/dankcalendar.svg"
+
+install -Dm644 "dankcalendar.svg" "/usr/share/icons/hicolor/scalable/apps/dankcalendar.svg"
 install -Dm644 "${DESKTOP_ID}.desktop" "/usr/share/applications/${DESKTOP_ID}.desktop"
 install -Dm644 "dcal.service" "/usr/lib/systemd/user/dcal.service"
-install -Dm644 "LICENSE" "/usr/share/licenses/dankcalendar/LICENSE"
+install -Dm644 "LICENSE" "/usr/share/licenses/dankcalendar-bin/LICENSE"
+install -Dm644 "README.md" "/usr/share/doc/dankcalendar-bin/README.md"
 cd ..
 rm -rf "$TMP_DIR"
 
